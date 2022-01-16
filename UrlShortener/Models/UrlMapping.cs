6aeefea915902c38b4bf2d566bf6a4d673c309dc
Daniel <Pleskach.Daniel@yandex.ru>
2022-01-16 19:32:41 +0700

    First commit

---
+++ UrlShortener/Models/UrlMapping.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.Models
+{
+    public class UrlMapping
+    {
+        
+        // Key for shortened url, uses first 8 characters of GUID.
+        [Key]
+        public string ShortenedUrlKey
+        {
+            get;
+            set;
+        }
+        
+        // Original URL. 
+        public string OriginalUrl
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ UrlShortener/Models/UrlMappingsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace UrlShortener.Models
+{
+    public class UrlMappingsContext : DbContext
+    {
+        public DbSet<UrlMapping> UrlMappings { get; set; }
+        
+        public UrlMappingsContext(DbContextOptions<UrlMappingsContext> options) : base(options)
+        { 
+            Database.EnsureCreated();
+        }
+        
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlite("Filename=UrlMappings.db");
+        // }
+    }
+}
